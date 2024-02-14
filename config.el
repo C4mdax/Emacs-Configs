97a4fe510bfd8fc8a4518ea8a302cd2467c19aa5
Camdax <147901536+C4mdax@users.noreply.github.com>
2024-02-13 18:34:01 -0600

    Add files via upload

---
+++ config.el
@@ -0,0 +1,94 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+(defun my-insert-mode-keys ()
+  "Define key bindings for insert mode."
+  (define-key evil-insert-state-map (kbd "C-S-c") 'clipboard-kill-ring-save)
+  (define-key evil-insert-state-map (kbd "C-S-v") 'clipboard-yank))
+
+(add-hook 'evil-insert-state-entry-hook #'my-insert-mode-keys)
+
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets. It is optional.
+ (setq user-full-name "C4mdax"
+       user-mail-address "c4mdax@ciencias.unam.mx")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom:
+;;
+;; - `doom-font' -- the primary font to use
+;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
+;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;; - `doom-symbol-font' -- for symbols
+;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
+
+;; See 'C-h v doom-font' for documentation and more examples of what they
+;; accept. For example:
+(setq-default cursor-type 'box)
+
+(setq doom-font (font-spec :family "JetBrains Mono NL SemiBold" :size 13))
+;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
+;;
+;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
+;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
+;; refresh your font settings. If Emacs still can't find your font, it likely
+;; wasn't installed correctly. Font issues are rarely Doom issues!
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+
+
+
+(add-to-list 'custom-theme-load-path "~/.doom.d/themes/themes/themes")
+(setq doom-theme 'doom-outrun-electric)
+
+
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type 'relative)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
+
+
+;; Whenever you reconfigure a package, make sure to wrap your config in an
+;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
+;;
+;;   (after! PACKAGE
+;;     (setq x y))
+;;
+;; The exceptions to this rule:
+;;
+;;   - Setting file/directory variables (like `org-directory')
+;;   - Setting variables which explicitly tell you to set them before their
+;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
+;;   - Setting doom variables (which start with 'doom-' or '+').
+;;
+;; Here are some additional functions/macros that will help you configure Doom.
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
+;; etc).
+;; Configuración de tamaño y posición de la ventana
+;;(setq initial-frame-alist '((left . 0) (top . 0) (width . 120) (height . 60)))
+;;(setq default-frame-alist '((left . 0) (top . 0) (width . 120) (height . 80)))
+
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
---
+++ custom.el
@@ -0,0 +1,18 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("" "" "3cdd0a96236a9db4e903c01cb45c0c111eb1492313a65790adb894f9f1a33b2d" default))
+ '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Source Code Pro" :foundry "ADBO" :slant normal :weight medium :height 97 :width normal)))))
+(put 'customize-changed 'disabled nil)
+(put 'customize-saved 'disabled nil)
+(put 'customize-save-customized 'disabled nil)
+(put 'upcase-region 'disabled nil)
